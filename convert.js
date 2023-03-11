de9abad6d37fa4169dfaf3b75a74c283fc7dddcb
daniel <adtdaniel87@gmail.com>
2023-03-10 23:27:44 -0600

    Se agregaron mensajes de error y se esta intentando otra manera para ver porque no muestra la cantidad convertida

--- convert.js
+++ convert.js
@@ -1,17 +1,31 @@
 function convert() {
-    const cantidad = document.getElementById("cantidad").value;
-    const dMoneda = document.getElementById("dMoneda").value;
-    const aMoneda = document.getElementById("aMoneda").value;
+  const cantidad = document.getElementById("cantidad").value;
+  const dMoneda = document.getElementById("dMoneda").value;
+  const aMoneda = document.getElementById("aMoneda").value;
 
-    const url = `https://api.exchangeratesapi.io/latest?base=${dMoneda}&symbols=${aMoneda}&access_key=kx0FIg023KgNdPZso1uUeOGSQUTisZ2T`;
+  const url = `https://api.exchangeratesapi.io/latest?base=${dMoneda}&symbols=${aMoneda}&access_key=kx0FIg023KgNdPZso1uUeOGSQUTisZ2T`;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const rate = data.rates[aMoneda];
-            const cantiConvertida = cantidad * rate;
-
-            document.getElementById("cantConvertida").innerHTML = `${cantiConvertida} ${aMoneda}`;
-        })
-        .catch(error => console.error(error));
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Se produjo un error al obtener el tipo de cambio.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (!data.rates || !data.rates[aMoneda]) {
+        throw new Error(
+          `No se pudo obtener el tipo de cambio para la moneda ${aMoneda}.`
+        );
+      }
+      const rate = data.rates[aMoneda];
+      const cantiConvertida = cantidad * rate;
+      document.getElementById(
+        "cantConvertida"
+      ).textContent = `${cantiConvertida} ${aMoneda}`;
+    })
+    .catch((error) => {
+      console.error(error);
+      alert(error.message);
+    });
 }
